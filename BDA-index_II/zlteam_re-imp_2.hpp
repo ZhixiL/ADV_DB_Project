a3f1fcd3cc1500361a14bb1ab02b638b824df3fb
ZhixiL <77127365+ZhixiL@users.noreply.github.com>
2023-11-17 15:23:02 -0500

    Updated code location, all of our implementation will be in a single file for each of the Index method

---
+++ BDA-index_II/zlteam_re-imp_2.hpp
@@ -0,0 +1,9 @@
+#include "stream.h"
+#include "uint40.h"
+#include <math.h>
+#include "bda-index_II.h"
+#include <sdsl/bit_vectors.hpp>
+#include <sdsl/rmq_support.hpp>	
+#include <sdsl/io.hpp>
+using namespace std;
+using namespace sdsl;
